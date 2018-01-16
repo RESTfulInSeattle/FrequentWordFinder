426f40418dc8da322792078c8422596e8b715fef
David Clayton <david@davidclayton.me>
2018-01-15 19:26:38 -0800

    WordCollector and WordRanker Created

---
+++ FrequentWordConsole/WordCollector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace FrequentWordConsole
+{
+    class WordCollector
+    {
+        public Dictionary<string, long> GetWords(string input, char[] delimiters)
+        {
+            Dictionary<string,long> words = new Dictionary<string, long>();
+
+            if (input.Length > 0 && delimiters.Length > 0)
+            {
+                string[] splitWords = input.Split(delimiters);
+
+                foreach (string word in splitWords )
+                {
+                    if (words.ContainsKey(word))
+                    {
+                        words[word]++;
+                    }
+                    else
+                    {
+                        words.Add(word,1);
+                    }
+                }
+            }
+
+            return words;
+
+        }
+    }
+}
---
+++ FrequentWordConsole/WordRanker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FrequentWordConsole
+{
+    class WordRanker
+    {
+        public Dictionary<string,long> RankedWords(Dictionary<string, long> words, int rank)
+        {
+            Dictionary<string,long> returnDictionary = new Dictionary<string, long>();
+
+            if (words.Keys.Count > 0 && rank > 0)
+            {
+                var sortedWords = words.ToList();
+                sortedWords.Sort((pair1, pair2) => pair2.Value.CompareTo(pair1.Value));
+
+                returnDictionary = (Dictionary<string, long>) sortedWords.Take(rank);
+            }
+
+            return returnDictionary;
+        }
+    }
+}
